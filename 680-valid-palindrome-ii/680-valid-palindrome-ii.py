c3270eccd3062a9032241b72674adcd179bd6b14
muffycoder <pratap.alok.raj@gmail.com>
2022-01-05 09:34:31 +0530

    Time: 170 ms (43.51%), Space: 14.7 MB (39.11%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            elif s[i+1:j+1] == s[i+1:j+1][::-1] or s[i:j] == s[i:j][::-1]:
+                return True
+            else:
+                return False
+        return True
