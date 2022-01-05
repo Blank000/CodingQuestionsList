f30bff31f3ba78b3db92b79623120f9045a758fa
muffycoder <pratap.alok.raj@gmail.com>
2022-01-05 09:22:12 +0530

    Time: 42 ms (15.71%), Space: 14.1 MB (82.43%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -1,18 +1,10 @@
 class Solution:
-    def maximumSwap(self, num: int) -> int:
-        # Maintaing a maximum from the right to left and doing a string comparison will help
-        num = list(str(num))
-        maxm = [len(num)-1]
-        for i in range(len(num)-2, -1, -1):
-            maxmUntilNow = int(num[maxm[0]])
-            if int(num[i]) > maxmUntilNow:
-                maxm = [i]+maxm
-            else:
-                maxm = [maxm[0]] + maxm
-        #print(maxm)
-        for i in range(len(num)):
-            if maxm[i] != i and int(num[i]) < int(num[maxm[i]]):
-                num[i], num[maxm[i]] = num[maxm[i]], num[i]
-                #print(i, maxm[i], num)
-                break
-        return int("".join(num))
+    def maximumSwap(self, num):
+        A = list(map(int, str(num)))
+        last = {x: i for i, x in enumerate(A)}
+        for i, x in enumerate(A):
+            for d in range(9, x, -1):
+                if last.get(d, -1) > i:
+                    A[i], A[last[d]] = A[last[d]], A[i]
+                    return int("".join(map(str, A)))
+        return num
