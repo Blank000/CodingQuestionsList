3cefb289e50b4375b8bf223d0de54fdbd2d09e44
muffycoder <pratap.alok.raj@gmail.com>
2022-01-14 12:35:10 +0530

    Time: 2414 ms (7.65%), Space: 59.1 MB (35.42%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,14 +1,33 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        count = 0
+        nonOverlapping = 0
+        endMin = float("inf")
+        points.sort()
+        for p in points:
+            if p[0] > endMin:
+                count += 1
+                nonOverlapping = 1
+                endMin = p[1]
+            else:
+                nonOverlapping += 1
+                endMin = min(endMin, p[1])
+        return count + 1 
     
-class Solution(object):
-    def findMinArrowShots(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
-        points = sorted(points, key = lambda x: x[1])
-        res, end = 0, -float('inf')
-        for interval in points:
-            if interval[0] > end:
-                res += 1
-                end = interval[1]
-        return res
+'''
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        count = 0
+        nonOverlapping = 1
+        endMin = float("inf")
+        points.sort()
+        for p in points:
+            if p[0] >= endMin:
+                count += (nonOverlapping-1)
+                nonOverlapping = 1
+                endMin = p[1]
+            else:
+                nonOverlapping += 1
+                endMin = min(endMin, p[1])
+        return count + nonOverlapping - 1
+'''
