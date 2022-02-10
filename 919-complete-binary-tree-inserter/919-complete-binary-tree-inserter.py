eebbb3f7e71799186f1c5bbe074fc4d6f81c8d26
muffycoder <pratap.alok.raj@gmail.com>
2022-02-10 06:08:02 +0530

    Time: 91 ms (58.73%), Space: 15.1 MB (41.76%) - LeetHub

--- 919-complete-binary-tree-inserter/919-complete-binary-tree-inserter.py
+++ 919-complete-binary-tree-inserter/919-complete-binary-tree-inserter.py
@@ -1,42 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class CBTInserter:
-
-    def __init__(self, root: Optional[TreeNode]):
+class CBTInserter(object):
+    def __init__(self, root):
+        self.deque = collections.deque()
         self.root = root
-    
-    
-    def bfsTraversal(self, node, val):
-        arr = [node]
-        temp = []
-        while arr:
-            for node in arr:
-                if node.left is None:
-                    node.left = TreeNode(val)
-                    return node.val
-                if node.right is None:
-                    node.right = TreeNode(val)
-                    return node.val
-                else:
-                    temp.append(node.left)
-                    temp.append(node.right)
-            arr = temp
-        return -1
-        
-        
-    def insert(self, val: int) -> int:
-        #height = self.getHeight(self.root)
-        return self.bfsTraversal(self.root, val)
+        q = collections.deque([root])
+        while q:
+            node = q.popleft()
+            if not node.left or not node.right:
+                self.deque.append(node)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
 
-    def get_root(self) -> Optional[TreeNode]:
+    def insert(self, v):
+        node = self.deque[0]
+        self.deque.append(TreeNode(v))
+        if not node.left:
+            node.left = self.deque[-1]
+        else:
+            node.right = self.deque[-1]
+            self.deque.popleft()
+        return node.val
+
+    def get_root(self):
         return self.root
-
-
-# Your CBTInserter object will be instantiated and called as such:
-# obj = CBTInserter(root)
-# param_1 = obj.insert(val)
-# param_2 = obj.get_root()
