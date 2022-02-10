6949fffdfb787ce6ece36f1c2812d988ef655f12
muffycoder <pratap.alok.raj@gmail.com>
2022-02-10 06:51:42 +0530

    Time: 339 ms (47.70%), Space: 16.6 MB (93.61%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,14 +1,14 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        for i in range(1, len(nums)):
-            nums[i] += nums[i-1]
-        dic = {}
+        # This is the question where we will use the dictionary approach of prefix sum array
+        dic = collections.defaultdict(int)
+        ps = 0
+        count = 0
         dic[0] = 1
-        res = 0
-        for i in range(len(nums)):
-            if (nums[i]-k) in dic:
-                res += dic[nums[i]-k]
-            dic[nums[i]] = dic.get(nums[i], 0) + 1
-        
-        return res
+        for n in nums:
+            ps += n
+            if (ps-k) in dic:
+                count += dic[ps-k]
+            dic[ps] += 1
+        return count
                 
