343c8b915eca0881abb8b4f1e08854da673b52e8
muffycoder <pratap.alok.raj@gmail.com>
2022-02-13 08:21:31 +0530

    Time: 55 ms (31.85%), Space: 14.2 MB (69.11%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,20 +1,10 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(first = 0, curr = []):
-            # if the combination is done
-            if len(curr) == k:  
-                output.append(curr[:])
-                return
-            for i in range(first, n):
-                # add nums[i] into the current combination
-                curr.append(nums[i])
-                # use next integers to complete the combination
-                backtrack(i + 1, curr)
-                # backtrack
-                curr.pop()
-        
         output = []
         n = len(nums)
-        for k in range(n + 1):
-            backtrack()
+        nth_bit = 1 << n
+        for i in range(2**n):
+            # generate bitmask, from 0..00 to 1..11
+            bitmask = bin(i | nth_bit)[3:]
+            output.append([nums[j] for j in range(len(nums)) if bitmask[j] == "1"])
         return output
