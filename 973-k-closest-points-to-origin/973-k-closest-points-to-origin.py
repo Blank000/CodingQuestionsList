346b55763f9c60fcb89437b36c30eecf5d9e6d7a
muffycoder <pratap.alok.raj@gmail.com>
2022-02-14 21:31:50 +0530

    Time: 1328 ms (20.63%), Space: 20.4 MB (26.24%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -2,13 +2,32 @@ import heapq
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap = []
-        heapq.heapify(heap)
+        if len(points) == k:
+            return points
+        distance = []
+        maxmDist = 0
         for a, b in points:
-            heapq.heappush(heap, (a**2+b**2, a, b))
+            distance.append([a**2+b**2, a, b])
+            maxmDist = max(maxmDist, distance[-1][0])
+        low, high = 0, maxmDist
         res = []
         while k:
-            _, a, b = heapq.heappop(heap)
-            res.append([a,b])
-            k -= 1
-        return res
+            mid = low + (high - low)//2
+            closestTemp, farthestTemp = [], []
+            for i in range(len(distance)):
+                d, a, b, = distance[i]
+                if d <= mid:
+                    closestTemp.append([d, a, b])
+                else:
+                    farthestTemp.append([d, a, b])
+            
+            if len(closestTemp) > k:
+                distance = closestTemp
+                high = mid
+            else:
+                k -= len(closestTemp)
+                res.extend(closestTemp)
+                distance = farthestTemp
+                low = mid
+            
+        return [[a,b] for _, a, b in res]
