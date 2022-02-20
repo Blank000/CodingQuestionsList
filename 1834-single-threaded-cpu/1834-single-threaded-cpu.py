a5587418b039bd46f8a7c094de65b521534c75e1
muffycoder <pratap.alok.raj@gmail.com>
2022-02-20 16:23:02 +0530

    Time: 2414 ms (66.25%), Space: 62.9 MB (53.40%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -11,18 +11,14 @@ class Solution:
         res = []
         i = 1
         while i < len(tasks) or heap:
-            #print(time,heap,tasks,tasks[i][0])
             timeToComplete, idx = heapq.heappop(heap)
             time += timeToComplete
             res.append(idx)
             while i < len(tasks) and tasks[i][0] <= time:
-                #print(heap, time, tasks[i])
                 heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
                 i += 1
             if len(heap) == 0 and i < len(tasks):
                 time = tasks[i][0]
                 heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
-                
                 i += 1
-            #print(heap, time, tasks[i])
         return res
