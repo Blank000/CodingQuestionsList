961c450581af9bf3939e2cdea5152e056e1d7e66
muffycoder <pratap.alok.raj@gmail.com>
2022-03-02 04:54:27 +0530

    Time: 4602 ms (40.71%), Space: 50.8 MB (6.70%) - LeetHub

---
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,31 @@
+class Solution:
+	def countSmaller(self, nums: List[int]) -> List[int]:
+		smallCount = [0]*len(nums)
+		def mergeSort(nums, start, end):
+			if start >= end:
+				return
+			mid = start + (end-start)//2
+			mergeSort(nums, start, mid)
+			mergeSort(nums, mid+1, end)
+			merge(nums, start, mid, end)
+		def merge(nums, start, mid, end):
+			temp = []
+			i, j = start, mid+1
+			while i <= mid:
+				while i <= mid and j <= end and nums[j][0] < nums[i][0]:
+					temp.append((nums[j][0], nums[j][1]))
+					j += 1
+				smallCount[nums[i][1]] += j-mid-1
+				temp.append((nums[i][0], nums[i][1]))
+				i += 1
+			while j <= end:
+				temp.append((nums[j][0], nums[j][1]))
+				j += 1
+			for i in range(len(temp)):
+				nums[i+start] = temp[i]
+		for i in range(len(nums)):
+			nums[i] = (nums[i], i)
+		mergeSort(nums, 0, len(nums)-1)
+		
+		return smallCount
+
