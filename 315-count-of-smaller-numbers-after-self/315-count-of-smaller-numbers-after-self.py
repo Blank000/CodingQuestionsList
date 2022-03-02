5ca34eb3cb180b5869ccd4280b070842547f45bb
muffycoder <pratap.alok.raj@gmail.com>
2022-03-02 14:27:25 +0530

    Time: 4954 ms (34.49%), Space: 52.1 MB (5.96%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -13,7 +13,7 @@ class Solution:
 			i, j = start, mid+1
 			while i <= mid:
 				while i <= mid and j <= end and nums[j][0] < nums[i][0]:
-					temp.append((nums[j][0], nums[j][1]))
+					temp.append((nums[j][0],nums[j][1]))
 					j += 1
 				smallCount[nums[i][1]] += j-mid-1
 				temp.append((nums[i][0], nums[i][1]))
@@ -25,6 +25,7 @@ class Solution:
 				nums[i+start] = temp[i]
 		for i in range(len(nums)):
 			nums[i] = (nums[i], i)
+		print(nums)
 		mergeSort(nums, 0, len(nums)-1)
 		
 		return smallCount
