10766b97d28f57c34d299217736b3f82917079f8
muffycoder <pratap.alok.raj@gmail.com>
2022-03-03 18:33:36 +0530

    Time: 752 ms (36.21%), Space: 14 MB (96.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,18 @@
 class Solution:
 	def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-		# First is the brute force method
-		dp = [[0]*(len(s1)+1)  for _ in range(len(s2)+1)]
-		for i in range(len(s2)+1):
-			dp[i][0] = 0
-		for i in range(len(s1)+1):
-			dp[0][i] = 0
+		# Second solution that we are presenting here is a space optimised solution
+		prev = [0]*(len(s1)+1)
 		maxm = 0
-		for i in range(1,len(s2)+1):
-			for j in range(1,len(s1)+1):
-				dp[i][j] = max(max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])
-				if s1[j-1] == s2[i-1]:
-					dp[i][j] = dp[i-1][j-1] + 1
-					maxm = max(maxm, dp[i][j])
+		for i in range(1, len(s2)+1):
+			curr = [0]*(len(s1)+1)
+			for j in range(1, len(s1)+1):
+				curr[j] = max(curr[j-1], max(prev[j-1], prev[j]))
+				if s2[i-1] == s1[j-1]:
+					curr[j] = prev[j-1] + 1
+					maxm = max(curr[j], maxm)
+			prev = curr
 		return maxm
+		
 			
 		
         
