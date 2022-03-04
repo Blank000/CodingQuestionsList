646d3df48581345abebda0e780991e896b487a56
muffycoder <pratap.alok.raj@gmail.com>
2022-03-04 08:00:38 +0530

    Time: 64 ms (8.09%), Space: 14 MB (52.28%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,10 +1,5 @@
+import math
 class Solution:
 	def numTrees(self, n: int) -> int:
-		if n == 0 or n == 1:
-			return 1
-		num = 1
-		den = 1
-		for k in range(2, n+1):
-			num *= (n+k)
-			den *= k
-		return (num//den)
+		return math.comb(2*n, n)//(n+1)
+
