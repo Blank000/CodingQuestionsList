82e67fcfec67db18c1d8a89a0b31b44110ee386d
muffycoder <pratap.alok.raj@gmail.com>
2022-03-04 21:05:48 +0530

    Time: 2175 ms (55.67%), Space: 30.4 MB (87.74%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -1,12 +1,14 @@
 class Solution:
-    def longestPalindromeSubseq(self, s1: str) -> int:
-        s2 = s1[::-1]
-        dp = [[0 for _ in range(len(s1)+1)] for _ in range(len(s2)+1)]
-        
-        for i in range(1, len(s2)+1):
-            for j in range(1, len(s1)+1):
-                if s1[i-1] == s2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]+1
-                else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[-1][-1]
+	def longestPalindromeSubseq(self, s1: str) -> int:
+		# Same as LCS with the reversed string but now we are making use of the fact that we are searching for pallindromes
+		dp = [[0]*(len(s1)+1) for _ in range(len(s1)+1)]
+		for i in range(len(s1)):
+			dp[i][i] = 1
+		for i in range(len(s1)-1, -1, -1):
+			for j in range(i+1, len(s1)):
+				if s1[i] == s1[j]:
+					dp[i][j] = dp[i+1][j-1] + 2
+				else:
+					dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+		return dp[0][len(s1)-1]
+
