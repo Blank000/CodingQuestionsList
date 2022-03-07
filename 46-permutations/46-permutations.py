a2a35b5825320d661d87cd37c41398ed42601f4c
muffycoder <pratap.alok.raj@gmail.com>
2022-03-07 08:08:45 +0530

    Time: 70 ms (25.74%), Space: 14.2 MB (52.42%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,12 +1,12 @@
 class Solution:
 	def permute(self, nums: List[int]) -> List[List[int]]:
-		res = []
-		def order(nums, idx):
-			if idx == len(nums):
-				res.append(nums[:])
-			for j in range(idx, len(nums)):
-				nums[idx], nums[j] = nums[j], nums[idx]
-				order(nums, idx+1)
-				nums[idx], nums[j] = nums[j], nums[idx]
-		order(nums, 0)
-		return res
+		result = []
+		def backtrack(arr, left, right):
+			if left == len(arr):
+				result.append(arr[:])
+			for i in range(left, right+1):
+				arr[left], arr[i] = arr[i], arr[left]
+				backtrack(arr, left+1, right)
+				arr[left], arr[i] = arr[i], arr[left]
+		backtrack(nums, 0, len(nums)-1)
+		return result
