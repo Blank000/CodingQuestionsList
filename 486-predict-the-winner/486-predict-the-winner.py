e774e5d676240b2be1249423ac963df0d9e4ba2f
muffycoder <pratap.alok.raj@gmail.com>
2022-03-09 07:26:06 +0530

    Time: 75 ms (28.75%), Space: 14 MB (75.11%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -3,9 +3,10 @@ class Solution:
 		dp = [[0 for _ in range(len(nums))] for _ in range(len(nums))]
 		for i in range(len(nums)):
 			dp[i][i] = nums[i]
-		for l in range(1, len(nums)):
+		for i in range(len(nums)-1):
+			dp[i][i+1] = max(nums[i], nums[i+1])
+		for l in range(2, len(nums)):
 			for i in range(len(nums)-l):
-				dp[i][i+l] = max(nums[i] - dp[i+1][i+l], nums[i+l] - dp[i][i+l-1])
-		return dp[0][len(nums)-1] >= 0
-
+				dp[i][i+l] = max(nums[i] + min(dp[i+2][i+l], dp[i+1][i+l-1]), nums[i+l] + min(dp[i+1][i+l-1], dp[i][i+l-2]))
+		return dp[0][len(nums)-1] >= sum(nums) - dp[0][len(nums)-1] 
 
