f1861897f7fc4a5742e442e0fe70b21c56a8959c
muffycoder <pratap.alok.raj@gmail.com>
2022-03-11 06:35:23 +0530

    Time: 1084 ms (18.08%), Space: 14.2 MB (93.73%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,14 +1,18 @@
 class Solution:
-    def longestStrChain(self, words: List[str]) -> int:
-        wordSet = set(words)
-
-        @lru_cache(None)
-        def dp(word):
-            ans = 1
-            for i in range(len(word)):
-                predecessor = word[:i] + word[i + 1:]
-                if predecessor in wordSet:
-                    ans = max(ans, dp(predecessor) + 1)
-            return ans
-
-        return max(dp(w) for w in words)
+	def longestStrChain(self, words: List[str]) -> int:
+		words.sort(key = len)
+		dp = [1]*len(words)
+		currLen = len(words[0])
+		startIdx, lastIdx = 0, 0
+		for i in range(len(words)):
+			if len(words[i]) > currLen:
+				startIdx, lastIdx = lastIdx, i
+				currLen = len(words[i])
+			for j in range(startIdx, lastIdx):
+				if len(words[i])-len(words[j]) == 1:
+					for k in range(len(words[i])):
+						if words[i][:k]+words[i][k+1:] == words[j]:
+							dp[i] = max(dp[i], dp[j] + 1)
+				else:
+					break
+		return max(dp)
